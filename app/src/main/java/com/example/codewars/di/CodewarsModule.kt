573cf7cf435f74af157d7b6dc0adaae4e64ac869
Joao Ferreira <maneca59@hotmail.com>
2023-05-28 23:26:31 +0100

    Add logging interceptor

--- app/src/main/java/com/example/codewars/di/CodewarsModule.kt
+++ app/src/main/java/com/example/codewars/di/CodewarsModule.kt
@@ -1,6 +1,7 @@
 package com.example.codewars.di
 
 import android.app.Application
+import com.example.codewars.BuildConfig
 import com.example.codewars.data.local.CodewarsDatabase
 import com.example.codewars.data.remote.CodewarsApi
 import com.example.codewars.data.repository.CodewarsRepositoryImp
@@ -13,7 +14,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 
@@ -28,10 +32,26 @@ class CodewarsModule {
 
     @Provides
     @Singleton
-    fun provideChallengesApi(): CodewarsApi {
+    fun provideOkHttpClient(): OkHttpClient{
+        val okHttpClientBuilder = OkHttpClient.Builder()
+            .connectTimeout(40, TimeUnit.SECONDS)
+            .readTimeout(40, TimeUnit.SECONDS)
+        if (BuildConfig.DEBUG) {
+            val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+            okHttpClientBuilder.addInterceptor(httpLoggingInterceptor)
+        }
+        return okHttpClientBuilder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideChallengesApi(client: OkHttpClient): CodewarsApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(CodewarsApi::class.java)
     }
